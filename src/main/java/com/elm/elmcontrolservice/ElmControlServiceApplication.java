cc48db37837285943ccb97bf26d33fbbc1409065
SubhajitHelloWorld <subhajitbhattacharjee007@gmail.com>
2024-03-19 23:55:52 +0530

    Initial commit

---
+++ src/main/java/com/elm/elmcontrolservice/ElmControlServiceApplication.java
@@ -0,0 +1,14 @@
+package com.elm.elmcontrolservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElmControlServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElmControlServiceApplication.class, args);
+		System.out.println("Hello World-1");
+	}
+
+}
---
+++ src/test/java/com/elm/elmcontrolservice/ElmControlServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elm.elmcontrolservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElmControlServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
